5d0886afd9f1946b3596897dedb702ad591c2092
Ammar Daham <ammar.daham@metropolia.fi>
2022-12-12 23:58:46 +0200

    github pipline added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { Provider } from 'react-redux'
-import { store } from '../../fs12-fullstack/client/src/redux/store'
-import App from '../../fs12-fullstack/client/src/components/App'
+import App from './App'
 
 test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  )
-
-  expect(getByText('')).toBeInTheDocument()
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/Countries/i)
+  expect(linkElement).toBeInTheDocument()
 })
