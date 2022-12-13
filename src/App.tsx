504607994c9c82b1318dc671ae2a3151f0a8e1c1
Ammar Daham <ammar.daham@metropolia.fi>
2022-12-13 02:01:17 +0200

    conflict fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,16 @@ import Routes from './Routes'
 import ThemeContext from './context/themeProvider'
 
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 
 export default function App() {
   const [theme, setTheme] = useState('light')
 
   return (
     <>
-      <BrowserRouter>
-        <ThemeContext.Provider value={{ theme, setTheme }}>
-          <Header />
-          <Routes />
-        </ThemeContext.Provider>
-      </BrowserRouter>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <Header />
+        <Routes />
+      </ThemeContext.Provider>
     </>
   )
 }
